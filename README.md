980bc6e3c603d66a20d5928ef621058ef633dd80
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2022-04-06 13:40:32 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 docker-compose down
 docker-compose up -d --build
+
+### Connect to container
+docker exec -it <container name> /bin/sh
+
+
+mysql --host=localhost --user=api --password=api api
--- api/index.php
+++ api/index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo "test";
+echo "Hello app php with docker";
+
+try {
+    $database = new PDO('mysql:host=mysql;dbname=api', 'api', 'api');
+    echo "<br> success connection to database";
+} catch (\Throwable $th) {
+    echo "eror connection to database";
+}
